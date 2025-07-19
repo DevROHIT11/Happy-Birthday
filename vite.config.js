e8bdd8fe8918d7846bdded899fb0ed7a16dabaff
Rohit Lahu Bhalekar <165887184+DevROHIT11@users.noreply.github.com>
2025-07-19 18:55:46 +0530

    updates

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Happy-Birthday/', // 👈 important for GitHub Pages
+  base: '/Happy-Birthday', // 👈 important for GitHub Pages
 })
